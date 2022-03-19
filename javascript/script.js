5691510657f4c118aa90f0e0f3da39d6a108d037
Negin Soleimani <negin.kdk1@gmail.com>
2022-03-19 16:46:33 +0330

    clean js code

--- javascript/script.js
+++ javascript/script.js
@@ -1,12 +1,14 @@
 const menuBtn = document.querySelector(".navbarMobile");
-const mobileMenu=document.querySelector(".mobileMenu")
+const mobileMenu = document.querySelector(".mobileMenu")
 const closeBtn = document.querySelector(".CloseBtn");
-const logo = document.querySelector(".logo");
- const mobileNavbarOpen = () =>{
+
+//method of open mobile menu
+const mobileNavbarOpen = () =>{
     menuBtn.style.display = "none";
     mobileMenu.style.display="block";
 }
 
+//method of close mobile menu
 const mobileNavbarClose = () =>{
     menuBtn.style.display = "block";
     mobileMenu.style.display="none";
